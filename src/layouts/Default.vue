3c58ae990f99163b8cca145c3e9dfd46b46ed7dd
David Li <davidli012345@gmail.com>
2022-12-12 05:04:17 +0000

    fix: auto ads

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,10 @@
 <template>
 <div>
   <Navbar />
+  <Adsense
+      data-ad-client="ca-pub-2479144310234386"
+      data-ad-slot="1234567890">
+  </Adsense>
   <transition name="fade" appear>
     <main> <!-- a wrapper for slot is needed -->
       <slot /> <!-- the content -->
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import Ads from 'vue-google-adsense'
 import DefaultLayout from '~/layouts/Default.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
@@ -10,6 +11,13 @@ export default function (Vue, { router, head, isClient }) {
     href: 'https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css'
   })
   Vue.use(Buefy)
+  if (isClient) {
+    Vue.use(require('vue-script2'));
+    Vue.use(require('vue-google-adsense/dist/Adsense.min.js'));
+    Vue.use(require('vue-google-adsense/dist/InArticleAdsense.min.js'));
+    Vue.use(require('vue-google-adsense/dist/InFeedAdsense.min.js'));
+    Vue.use(Ads.AutoAdsense, { adClient: 'ca-pub-2479144310234386', isNewAdsCode: true }) 
+  }
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
